5575e7a8c2a690a61cdba6d481aa051ebf6a4e96
Vanloukdom <151269579+Vanloukdom@users.noreply.github.com>
2026-02-21 14:25:30 +0100

    Initial commit

--- src/weather_app/config.py
+++ src/weather_app/config.py
@@ -1,4 +1,4 @@
 import os
 
-API_KEY = os.getenv("OPENWEATHER_API_KEY", "53c98839b6ac2512dc4480960b478521")
+API_KEY = os.getenv("OPENWEATHER_API_KEY", "")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
